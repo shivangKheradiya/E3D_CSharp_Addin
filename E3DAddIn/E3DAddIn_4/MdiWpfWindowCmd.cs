1b262eee0ae5a0882f496cce9968c7f64816304e
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2024-05-27 18:50:59 +0530

    E3DAddIn_4: WPF Forms compatibility added (#1)
    E3DAddIn_4 WPF Forms are running

---
+++ E3DAddIn/E3DAddIn_4/MdiWpfWindowCmd.cs
@@ -0,0 +1,60 @@
+﻿using Aveva.ApplicationFramework.Presentation;
+using System;
+using System.Windows.Forms.Integration;
+
+namespace E3DAddIn_4
+{
+    public class MdiWpfWindowCmd : Command
+    {
+        MdiWindow mdiWindow;
+
+        public MdiWpfWindowCmd(WindowManager windowManager)
+        {
+            base.Key = "E3DAddIn_4.MdiWpfWindowCmd";
+
+            ElementHost wpfElementHost = new ElementHost();
+            MyWpfUserControl myWpfUserControl = new MyWpfUserControl();
+            wpfElementHost.Child = myWpfUserControl;
+
+            mdiWindow = windowManager.CreateMdiWindow("CreateMdiWpfWindow", "MyMdiWpfWindow", wpfElementHost);
+            mdiWindow.Closing += MdiWindow_Closing;
+
+            this.ExecuteOnCheckedChange = false;
+            mdiWindow.Form.TopMost = true;
+        }
+
+        private void MdiWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.Checked = false;
+            mdiWindow.Hide();
+            e.Cancel = true;
+        }
+
+        private void WindowManager_WindowLayoutLoaded(object sender, EventArgs e)
+        {
+            this.Checked = mdiWindow.Visible;
+        }
+
+        public override void Execute()
+        {
+            try
+            {
+                if (mdiWindow.Visible)
+                {
+                    mdiWindow.Hide();
+                }
+                else
+                {
+                    mdiWindow.Show();
+                    mdiWindow.Float();
+                }
+                base.Execute();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
