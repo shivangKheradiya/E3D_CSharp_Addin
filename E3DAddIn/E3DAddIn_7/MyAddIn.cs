a2e07eab006bfd13b4102c9527b748f0cdccfef6
shivangKheradiya <shivangatul@gmail.com>
2024-06-02 01:16:49 +0530

    E3DAddIn_7 : PdmsException Feature Added
    PDMS Exceptions can be helpful when we need to check the exception occurred by which code

---
+++ E3DAddIn/E3DAddIn_7/MyAddIn.cs
@@ -0,0 +1,39 @@
+﻿using Aveva.ApplicationFramework;
+using Aveva.ApplicationFramework.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3DAddIn_7
+{
+    public class MyAddIn : IAddin
+    {
+        public string Name
+        {
+            get { return "MyAddin"; }
+        }
+
+        public string Description
+        {
+            get { return "My Test Addin"; }
+        }
+
+        public void Start(ServiceManager serviceManager)
+        {
+            CommandManager commandManager = (CommandManager)DependencyResolver.GetImplementationOf<ICommandManager>();
+
+            TextBoxCmd textBoxCmd = new TextBoxCmd();
+            ExceptionCmd exceptionCmd = new ExceptionCmd(textBoxCmd);
+
+            commandManager.Commands.Add(textBoxCmd);
+            commandManager.Commands.Add(exceptionCmd);
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
---
+++ E3DAddIn/E3DAddIn_7/TextBoxCmd.cs
@@ -0,0 +1,12 @@
+﻿using Aveva.ApplicationFramework.Presentation;
+
+namespace E3DAddIn_7
+{
+    public class TextBoxCmd : Command
+    {
+        public TextBoxCmd() 
+        {
+            base.Key = "E3DAddIn_7.TextBoxCmd";
+        }
+    }
+}
