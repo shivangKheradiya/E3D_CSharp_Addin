bfbc13826016b6146382f33b2a0aac6ed8c20281
shivangKheradiya <shivangatul@gmail.com>
2024-06-02 02:11:23 +0530

    E3DAddIn_8 : NetGridControl Added to Windows Panel Object

---
+++ E3DAddIn/E3DAddIn_8/GridCmd.cs
@@ -0,0 +1,52 @@
+﻿using Aveva.ApplicationFramework.Presentation;
+
+namespace E3DAddIn_8
+{
+    public class GridCmd : Command
+    {
+        private WindowManager windowsManager;
+        private DockedWindow gridWindows;
+
+        public GridCmd(WindowManager windowManager)
+        {
+            base.Key = "E3DAddIn_8.GridCmd";
+            this.windowsManager = windowManager;
+            this.gridWindows = this.windowsManager.CreateDockedWindow("Grid", "Grid", new NetGridAddinControl() , DockedPosition.Right);
+            this.gridWindows.SaveLayout = true;
+            
+            this.gridWindows.Closed += GridWindows_Closed;
+
+            this.windowsManager.WindowLayoutLoaded += WindowsManager_WindowLayoutLoaded;
+            this.ExecuteOnCheckedChange = false;
+        }
+
+        private void WindowsManager_WindowLayoutLoaded(object sender, System.EventArgs e)
+        {
+            this.Checked = this.gridWindows.Visible;
+        }
+
+        private void GridWindows_Closed(object sender, System.EventArgs e)
+        {
+            this.Checked = false;
+        }
+
+        public override void Execute()
+        {
+            try
+            {
+                if (this.gridWindows.Visible)
+                {
+                    this.gridWindows.Hide();
+                }
+                else
+                {
+                    this.gridWindows.Show();
+                }
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ E3DAddIn/E3DAddIn_8/MyAddIn.cs
@@ -0,0 +1,38 @@
+﻿using Aveva.ApplicationFramework;
+using Aveva.ApplicationFramework.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3DAddIn_8
+{
+    public class MyAddIn : IAddin
+    {
+        public string Name
+        {
+            get { return "MyAddin"; }
+        }
+
+        public string Description
+        {
+            get { return "My Test Addin"; }
+        }
+
+        public void Start(ServiceManager serviceManager)
+        {
+            WindowManager windowManager = (WindowManager)DependencyResolver.GetImplementationOf<IWindowManager>();
+            CommandManager commandManager = (CommandManager)DependencyResolver.GetImplementationOf<ICommandManager>();
+
+            GridCmd gridCmd = new GridCmd(windowManager);
+
+            commandManager.Commands.Add(gridCmd);
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
