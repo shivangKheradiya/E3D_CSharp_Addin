64d7289e3b9d2372812777394a554b54ad44593f
shivangKheradiya <shivangatul@gmail.com>
2024-06-11 00:43:26 +0530

    E3DAddIn_10: Get Variables and Set Variables Methods Added
    Get Variables : Used to Get the Data from PML Variables
    Set Variables : Used to Set the Data To PML Variables

---
+++ E3DAddIn/E3DAddIn_10/SetPMLVariablesCmd.cs
@@ -0,0 +1,34 @@
+﻿using Aveva.ApplicationFramework.Presentation;
+using ac = Aveva.Core.Utilities.CommandLine;
+
+namespace E3DAddIn_10
+{
+    public class SetPMLVariablesCmd : Command
+    {
+        public SetPMLVariablesCmd() 
+        {
+            base.Key = "E3DAddIn_10.SetPMLVariablesCmd";
+        }
+
+        public override void Execute() 
+        {
+            try
+            {
+                string cmdText = "$P Set Variable Method Started \n" +
+                                 "!!varString = |MyName| \n" +
+                                 "!!varBool   = true \n" +
+                                 "!!varReal   = 5 \n" ;
+
+                ac.Command command = ac.Command.CreateCommand(cmdText);
+                if (command.RunInPdms()) 
+                {
+                    ac.Command okCommand = ac.Command.CreateCommand("$P variables are set");
+                    okCommand.RunInPdms();
+                }
+            }
+            catch (System.Exception)
+            {
+            }
+        }
+    }
+}
