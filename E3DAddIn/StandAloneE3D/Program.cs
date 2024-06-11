ef0e5b405bfcbaedcc74dc324b8fa73a50921eab
shivangKheradiya <shivangatul@gmail.com>
2024-06-12 01:22:01 +0530

    StandAloneE3D : E3D Stand Alone Application

---
+++ E3DAddIn/StandAloneE3D/Program.cs
@@ -0,0 +1,33 @@
+﻿using Aveva.Core.Database;
+using Aveva.Core.Utilities.Messaging;
+using Aveva.E3D.Standalone;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StandAloneE3D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                Debugger.Launch();
+                Standalone.Start();
+                Standalone.Open("ProjAPS", "SYSTEM", "XXXXXX", "ALL", out PdmsMessage error);
+                MDB mDB = MDB.CurrentMDB;
+
+                Console.WriteLine(mDB.Name);
+
+                Standalone.Finish();
+			}
+			catch (Exception)
+			{
+			}
+        }
+    }
+}
